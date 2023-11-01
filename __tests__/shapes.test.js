550fb2c7fb20a33e76dffb3d1596c7d725b1c693
Ricochet227 <skyler7crawford@gmail.com>
2023-10-31 20:46:25 -0600

    npx jest passed
    passed the npx jest test in the integrated terminal

--- __tests__/shapes.test.js
+++ __tests__/shapes.test.js
@@ -1,23 +1,28 @@
 const { Circle, Square, Triangle } = require('../lib/shapes');
 
-test('Circle render method should return the correct SVG string with default color', () => {
-  const circle = new Circle();
-  expect(circle.render()).toEqual('<circle cx="50" cy="50" r="40" fill="black" />');
-});
+describe('Circle', () => {
+    test('renders correctly', () => {
+      const shape = new Circle();
+      var color =('blue')
+      shape.setColor(color);
+      expect(shape.render()).toEqual(`<circle cx="50%" cy="50%" r="100" height="100%" width="100%" fill="${color}">`);
+    });
+  });
 
-test('Square render method should return the correct SVG string with specified color', () => {
-  const square = new Square();
-  square.setColor('red');
-  expect(square.render()).toEqual('<rect x="10" y="10" width="80" height="80" fill="red" />');
-});
+  describe('Square', () => {
+      test('renders correctly', () => {
+        const shape = new Square();
+        var color =('green')
+        shape.setColor(color);
+        expect(shape.render()).toEqual(`<rect x="50" height="200" width="200" fill="${color}">`);
+      });
+    });
 
-test('Triangle render method should return the correct SVG string with default color', () => {
-  const triangle = new Triangle();
-  expect(triangle.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="black" />');
-});
-
-test('Triangle setColor method should set the color property correctly', () => {
-  const triangle = new Triangle();
-  triangle.setColor('blue');
-  expect(triangle.color).toEqual('blue');
-});
+  describe('Triangle', () => {
+      test('renders correctly', () => {
+        const shape = new Triangle();
+        var color =('pink')
+        shape.setColor(color);
+        expect(shape.render()).toEqual(`<polygon height="100%" width="100%" points="0,200 300,200 150,0" fill="${color}">`);
+      });
+    });
